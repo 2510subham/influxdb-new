46e014d79ae8f1c8cc57a9100b30a7de31f16940
Subham Agarwal <subhamagarwal400@gmail.com>
2023-03-14 13:01:46 +0530

    influx-db setup with mqtt

---
+++ influxdb.js
@@ -0,0 +1,60 @@
+//setting up mqtt with influx db 
+
+const mqtt = require("mqtt");
+
+const { InfluxDB, Point } = require('@influxdata/influxdb-client')
+
+const token = process.env.TOKEN
+
+const url = process.env.URL
+const org='nrxen';
+const bucket='nimmos'
+let client=new InfluxDB({url,token});
+
+const clients=mqtt.connect(process.env.MQ_URL,{
+    username: process.env.USER,
+    password: process.env.PASS,
+    port: process.env.PORT
+})
+clients.on("connect", async () => {
+     console.log("connected");
+    await clients.publish("nimmos", JSON.stringify({name:"nimmos",value:"route-engine"}));  
+});
+
+ clients.subscribe("nimmos");
+
+ clients.on("message", (topic, message) => {
+    console.log("message received", topic, message.toString());
+    const data=JSON.parse(message.toString());
+    console.log(data);
+    
+    const writeClient = client.getWriteApi(org, bucket,'ns');
+    //creating point
+    let point=new Point('names').tag(Object.keys(data)[0],Object.values(data)[0])
+    .tag(Object.keys(data)[1],Object.values(data)[1])
+    .stringField('nrxen',"hello");
+    writeClient.writePoint(point);//writing point to influxdb
+    writeClient.flush();//flushing the data to influxdb
+    console.log(point);
+
+});
+//seeing the data in influxdb
+let query=client.getQueryApi(org)
+let fluxquery=`form(bucket:"nimmos")
+|>  range(start:-1m)
+|>  filter(fn:(r)=>r._measurement=="names")`
+
+query.queryRows(fluxquery,{
+    next:(row,tableMeta)=>{
+        const tableObject = tableMeta.toObject(row)
+        console.log(row);
+        console.log(tableObject)
+    },
+    error:(error)=>{
+        console.error('\nError',error)
+    },
+    complete:()=>{
+        console.log('\nSuccess for Query1')
+    }
+
+})
